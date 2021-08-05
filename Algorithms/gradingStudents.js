3ef359ec4479d58e8185f84681a84e7cbd82d29f
Somnath Das <37792778+SomnathDas@users.noreply.github.com>
2021-08-05 13:54:27 +0530

    Update gradingStudents.js

--- Algorithms/gradingStudents.js
+++ Algorithms/gradingStudents.js
@@ -1,9 +1,49 @@
+/*
+Problem Name: Grading
+Problem Details: https://www.hackerrank.com/challenges/grading/problem
+Difficulty: Easy
+*/
+
+// Boilerplate
+"use strict";
+
+const fs = require("fs");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
+
+// Helper Function
 const nearestFiveMultiple = (number) => {
   let n = Math.ceil(number / 5);
   return n * 5;
 };
 
-/*function gradingStudents(grades) {
+// Main Function | Type-I
+function gradingStudents(grades) {
   let finalGrades = 0;
   let nearestFive = nearestFiveMultiple(grades);
   Math.abs(grades - nearestFive) < 3
@@ -14,7 +54,8 @@ const nearestFiveMultiple = (number) => {
   console.log(finalGrades);
   return finalGrades;
 }
-*/
+
+// Main Function | Type-II
 function gradingStudents(grades) {
   let finalGrades = 0;
   let nearestFive = nearestFiveMultiple(grades);
@@ -26,13 +67,22 @@ function gradingStudents(grades) {
   return finalGrades;
 }
 
-let testGrade0 = 73;
-let testGrade1 = 75;
-let testGrade2 = 67;
-let testGrade3 = 40;
-let testGrade4 = 33;
-gradingStudents(testGrade0);
-gradingStudents(testGrade1);
-gradingStudents(testGrade2);
-gradingStudents(testGrade3);
-gradingStudents(testGrade4);
+// Engine
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const gradesCount = parseInt(readLine().trim(), 10);
+
+  let grades = [];
+
+  for (let i = 0; i < gradesCount; i++) {
+    const gradesItem = parseInt(readLine().trim(), 10);
+    grades.push(gradesItem);
+  }
+
+  const result = gradingStudents(grades);
+
+  ws.write(result.join("\n") + "\n");
+
+  ws.end();
+}
